3fa6df0e455f737f1120c1ba716e08b93deb70c4
sigit <sigit_suryono25@hotmail.com>
2019-04-11 13:06:44 +0700

    Initial commit

---
+++ app/src/main/java/com/lauwba/surelabs/demofirebase/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.lauwba.surelabs.demofirebase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hello.onClick {
+            toast("Auch.. aku diklik")
+        }
+    }
+}
